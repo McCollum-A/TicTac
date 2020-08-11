71fbf356628e9936a5eb59d8cbaeb0f8f0ae11a6
McCollum-A <adwmccollum@gmail.com>
2020-08-10 21:12:44 -0700

    Bug fixes and code cleanup
    Testing showed that a player could overwrite a previous play (the grid choices were not locking). Added a new function and calls to prevent this, now each square can only be picked once.
    
    General code cleanup and improved comments were also performed.
    
    -Andrew

--- TicTac/Form1.Designer.vb
+++ TicTac/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FormGame
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class FormGame
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.LblSpot7 = New System.Windows.Forms.Label()
         Me.LblSpot8 = New System.Windows.Forms.Label()
@@ -34,15 +34,6 @@ Partial Class FormGame
         Me.BtnStart = New System.Windows.Forms.Button()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.LblPlayer = New System.Windows.Forms.Label()
-        Me.LblR1 = New System.Windows.Forms.Label()
-        Me.LblR2 = New System.Windows.Forms.Label()
-        Me.LblR3 = New System.Windows.Forms.Label()
-        Me.LblC1 = New System.Windows.Forms.Label()
-        Me.LblC2 = New System.Windows.Forms.Label()
-        Me.LblC3 = New System.Windows.Forms.Label()
-        Me.LblBS = New System.Windows.Forms.Label()
-        Me.LblFS = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'LblSpot7
@@ -154,101 +145,11 @@ Partial Class FormGame
         Me.LblPlayer.TabIndex = 10
         Me.LblPlayer.Text = "Player: _"
         '
-        'LblR1
-        '
-        Me.LblR1.AutoSize = True
-        Me.LblR1.Location = New System.Drawing.Point(32, 450)
-        Me.LblR1.Name = "LblR1"
-        Me.LblR1.Size = New System.Drawing.Size(39, 13)
-        Me.LblR1.TabIndex = 11
-        Me.LblR1.Text = "Label1"
-        '
-        'LblR2
-        '
-        Me.LblR2.AutoSize = True
-        Me.LblR2.Location = New System.Drawing.Point(156, 450)
-        Me.LblR2.Name = "LblR2"
-        Me.LblR2.Size = New System.Drawing.Size(39, 13)
-        Me.LblR2.TabIndex = 12
-        Me.LblR2.Text = "Label2"
-        '
-        'LblR3
-        '
-        Me.LblR3.AutoSize = True
-        Me.LblR3.Location = New System.Drawing.Point(326, 450)
-        Me.LblR3.Name = "LblR3"
-        Me.LblR3.Size = New System.Drawing.Size(39, 13)
-        Me.LblR3.TabIndex = 13
-        Me.LblR3.Text = "Label3"
-        '
-        'LblC1
-        '
-        Me.LblC1.AutoSize = True
-        Me.LblC1.Location = New System.Drawing.Point(35, 502)
-        Me.LblC1.Name = "LblC1"
-        Me.LblC1.Size = New System.Drawing.Size(39, 13)
-        Me.LblC1.TabIndex = 14
-        Me.LblC1.Text = "Label4"
-        '
-        'LblC2
-        '
-        Me.LblC2.AutoSize = True
-        Me.LblC2.Location = New System.Drawing.Point(156, 501)
-        Me.LblC2.Name = "LblC2"
-        Me.LblC2.Size = New System.Drawing.Size(39, 13)
-        Me.LblC2.TabIndex = 15
-        Me.LblC2.Text = "Label5"
-        '
-        'LblC3
-        '
-        Me.LblC3.AutoSize = True
-        Me.LblC3.Location = New System.Drawing.Point(329, 500)
-        Me.LblC3.Name = "LblC3"
-        Me.LblC3.Size = New System.Drawing.Size(39, 13)
-        Me.LblC3.TabIndex = 16
-        Me.LblC3.Text = "Label6"
-        '
-        'LblBS
-        '
-        Me.LblBS.AutoSize = True
-        Me.LblBS.Location = New System.Drawing.Point(40, 540)
-        Me.LblBS.Name = "LblBS"
-        Me.LblBS.Size = New System.Drawing.Size(39, 13)
-        Me.LblBS.TabIndex = 17
-        Me.LblBS.Text = "Label7"
-        '
-        'LblFS
-        '
-        Me.LblFS.AutoSize = True
-        Me.LblFS.Location = New System.Drawing.Point(156, 540)
-        Me.LblFS.Name = "LblFS"
-        Me.LblFS.Size = New System.Drawing.Size(39, 13)
-        Me.LblFS.TabIndex = 18
-        Me.LblFS.Text = "Label8"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(325, 539)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 19
-        Me.Label1.Text = "Label1"
-        '
         'FormGame
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(442, 565)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.LblFS)
-        Me.Controls.Add(Me.LblBS)
-        Me.Controls.Add(Me.LblC3)
-        Me.Controls.Add(Me.LblC2)
-        Me.Controls.Add(Me.LblC1)
-        Me.Controls.Add(Me.LblR3)
-        Me.Controls.Add(Me.LblR2)
-        Me.Controls.Add(Me.LblR1)
+        Me.ClientSize = New System.Drawing.Size(442, 442)
         Me.Controls.Add(Me.LblPlayer)
         Me.Controls.Add(Me.BtnStart)
         Me.Controls.Add(Me.LblSpot3)
@@ -279,13 +180,4 @@ Partial Class FormGame
     Friend WithEvents BtnStart As Button
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
     Friend WithEvents LblPlayer As Label
-    Friend WithEvents LblR1 As Label
-    Friend WithEvents LblR2 As Label
-    Friend WithEvents LblR3 As Label
-    Friend WithEvents LblC1 As Label
-    Friend WithEvents LblC2 As Label
-    Friend WithEvents LblC3 As Label
-    Friend WithEvents LblBS As Label
-    Friend WithEvents LblFS As Label
-    Friend WithEvents Label1 As Label
 End Class
--- TicTac/Form1.vb
+++ TicTac/Form1.vb
@@ -1,69 +1,42 @@
 ﻿Public Class FormGame
-    Public Playing As Boolean 'T/F to track if a game is active or not
+    Public Playing As Boolean 'Boolean to track if a game is active or not
     Public PlayerTurn As Integer 'Track player 1 "1" or Player 2 "2" 's turn
-    Public CurrentSpot As Object 'Which of the 9 spots of the board is selected, containing the label as an object
+    Public CurrentSpot As Object 'Which of the 9 label objects on the board is currently selected, containing the label as an object
     Public CurrentTurn As Integer 'Counter to track turns 1 through 9, as the board has 9 spots
-    Public GameOver As Boolean 'T/F flag for weither the game is over or not
-    '------------------
-    Public Grid(2, 2) As Integer
-    Public GridRow As Integer = 0
-    Public GridColumn As Integer = 0
-
+    Public GameOver As Boolean 'Boolean for wiether the game is over or not
+    Public Grid(2, 2) As Integer 'This 2D array mimics the game board to track win conditions via positive/negative integers
+    Public GridRow As Integer = 0 'Holding variable for communicate UI clicks to the win condition grid
+    Public GridColumn As Integer = 0 'Holding variable for communicate UI clicks to the win condition grid
     Private Sub FormGame_Load(sender As Object, e As EventArgs) Handles Me.Load
         Playing = False 'On load, the game is not being played
         GameOver = False 'On load, the game is not won
-        LblPlayer.Text = "Player: None" 'Sets text
-        CurrentTurn = 0 'Sets turn Count
-        LblSpot1.Tag = "0, 0"
-        LblSpot2.Tag = "1, 0"
-        LblSpot3.Tag = "2, 0"
-        LblSpot4.Tag = "0, 1"
-        LblSpot5.Tag = "1, 1"
-        LblSpot6.Tag = "2, 1"
-        LblSpot7.Tag = "0, 1"
-        LblSpot8.Tag = "1, 2"
-        LblSpot9.Tag = "2, 2"
+        LblPlayer.Text = "Player: None" 'On load, neither player is currently playing their turn
+        CurrentTurn = 0 'Sets turn count to zero
     End Sub
-    Private Sub PlayerMark() 'Private sub to be called when a player chooses which square to mark
-        ' MessageBox.Show(CurrentSpot.Tag)
-        If Playing = True And CurrentTurn < 9 And PlayerTurn = 1 Then
+    Private Sub PlayerMark() 'Sub to be called when a player chooses which game board square to mark
+        If Playing = True And CurrentTurn < 9 And PlayerTurn = 1 And CurrentSpot.text <> "O" Then 'playing the game, not past 9 turns, (X or O),  chosen spot isn't taken
             CurrentSpot.text = "X" '[selected label object].text property
             PlayerTurn = 2 'Set the next player's turn
             LblPlayer.Text = "Player: O" 'Display next player's turn
-        ElseIf Playing = True And CurrentTurn < 9 And PlayerTurn = 2 Then
+        ElseIf Playing = True And CurrentTurn < 9 And PlayerTurn = 2 And CurrentSpot.text <> "X" Then
             CurrentSpot.text = "O"
             PlayerTurn = 1
             LblPlayer.Text = "Player: X"
         Else
             Return 'If for some reason the if/then test fails, end this Sub
         End If
-
         CurrentTurn = CurrentTurn + 1 'Move the overall game turn counter up 1
-        'BtnStart.Text = CurrentTurn.ToString
         Call WinLogic() 'Call this Sub to check if this turn caused a player to win
-
-        'Below for Visual Debug, Remove upon release
-        LblR1.Text = Grid(0, 0)
-        LblR2.Text = Grid(1, 0)
-        LblR3.Text = Grid(2, 0)
-        LblC1.Text = Grid(0, 1)
-        LblC2.Text = Grid(1, 1)
-        LblC3.Text = Grid(2, 1)
-        LblBS.Text = Grid(0, 2)
-        LblFS.Text = Grid(1, 2)
-        Label1.Text = Grid(2, 2)
-
     End Sub
-
     Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
         If CurrentTurn = 0 Then 'Test if this is the first round of play
             Playing = True 'game is now being played
             PlayerTurn = 1 'set turn to turn 1
             LblPlayer.Text = "Player: X" 'Display current player's turn
-            BtnStart.Text = "Playing..."
-            BtnStart.Enabled = False
+            BtnStart.Text = "Playing..." 'Idle message for the start button
+            BtnStart.Enabled = False 'Disable the start button to prevent accidental restarts
         ElseIf GameOver = True Then 'Test if this is the 2nd or greater turn of play
-            LblSpot1.Text = "-" 'Set 9 spots to show "-" instead of X or O
+            LblSpot1.Text = "-" 'Set 9 board squares to show "-" instead of X or O
             LblSpot2.Text = "-"
             LblSpot3.Text = "-"
             LblSpot4.Text = "-"
@@ -72,38 +45,32 @@
             LblSpot7.Text = "-"
             LblSpot8.Text = "-"
             LblSpot9.Text = "-"
-            Playing = True
+            Playing = True 'The game is now being played again
             GameOver = False 'It is now a re-match, so gameover is false
             PlayerTurn = 1
             LblPlayer.Text = "Player: X"
             CurrentTurn = 0
-            ReDim Grid(2, 2)
+            ReDim Grid(2, 2) 'This is a quick way to "reset" the win-onditions array to all zeros
             BtnStart.Text = "Playing..."
             BtnStart.Enabled = False
-
         Else
-
         End If
-
-
     End Sub
-
     Private Sub LblSpot1_Click_1(sender As Object, e As EventArgs) Handles LblSpot1.Click
         CurrentSpot = LblSpot1 'Sets the CurrentSpot object holder as the clicked spot
-        GridColumn = 0
-        GridRow = 0
-        Call GameBoard()
+        GridColumn = 0 'Sets this holding variable to the correct array coordinants
+        GridRow = 0 'Sets this holding variable to the correct array coordinants
+        Call GameBoard() 'This sub does the actual work of modifying the win-conditions array
         Call PlayerMark() 'Call sub to activate the player's choice
     End Sub
-
     Private Sub LblSpot2_Click(sender As Object, e As EventArgs) Handles LblSpot2.Click
+        'The only differences between the 9 label _Click events are the GridColumn and GridRow integers
         CurrentSpot = LblSpot2
         GridColumn = 1
         GridRow = 0
         Call GameBoard()
         Call PlayerMark()
     End Sub
-
     Private Sub LblSpot3_Click(sender As Object, e As EventArgs) Handles LblSpot3.Click
         CurrentSpot = LblSpot3
         GridColumn = 2
@@ -111,7 +78,6 @@
         Call GameBoard()
         Call PlayerMark()
     End Sub
-
     Private Sub LblSpot4_Click(sender As Object, e As EventArgs) Handles LblSpot4.Click
         CurrentSpot = LblSpot4
         GridColumn = 0
@@ -119,7 +85,6 @@
         Call GameBoard()
         Call PlayerMark()
     End Sub
-
     Private Sub LblSpot5_Click(sender As Object, e As EventArgs) Handles LblSpot5.Click
         CurrentSpot = LblSpot5
         GridColumn = 1
@@ -127,7 +92,6 @@
         Call GameBoard()
         Call PlayerMark()
     End Sub
-
     Private Sub LblSpot6_Click(sender As Object, e As EventArgs) Handles LblSpot6.Click
         CurrentSpot = LblSpot6
         GridColumn = 2
@@ -135,7 +99,6 @@
         Call GameBoard()
         Call PlayerMark()
     End Sub
-
     Private Sub LblSpot7_Click(sender As Object, e As EventArgs) Handles LblSpot7.Click
         CurrentSpot = LblSpot7
         GridColumn = 0
@@ -143,7 +106,6 @@
         Call GameBoard()
         Call PlayerMark()
     End Sub
-
     Private Sub LblSpot8_Click(sender As Object, e As EventArgs) Handles LblSpot8.Click
         CurrentSpot = LblSpot8
         GridColumn = 1
@@ -151,7 +113,6 @@
         Call GameBoard()
         Call PlayerMark()
     End Sub
-
     Private Sub LblSpot9_Click(sender As Object, e As EventArgs) Handles LblSpot9.Click
         CurrentSpot = LblSpot9
         GridColumn = 2
@@ -159,10 +120,9 @@
         Call GameBoard()
         Call PlayerMark()
     End Sub
-
     Private Sub WinLogic()
-        'This is the game's victory logic. The 8 arrays represent the 8 lines you can fill up to win tic-tac-toe. So this if/then checks
-        'to see if any array contains 3 (an X equals 1) or -3 (an O equals -1), and therefore a player has won
+        'This is the game's victory logic. The 8 Integers below represent the 8 lines you can fill up to win tic-tac-toe.
+        'This If/Then checks to see if any integer equals 3 (an X equals 1) or -3 (an O equals -1), and therefore a player has won
         Dim Row1 As Integer = Grid(0, 0) + Grid(1, 0) + Grid(2, 0)
         Dim Row2 As Integer = Grid(0, 1) + Grid(1, 1) + Grid(2, 1)
         Dim Row3 As Integer = Grid(0, 2) + Grid(1, 2) + Grid(2, 2)
@@ -176,14 +136,14 @@
             GameOver = True 'Sets the game as over
             Playing = False 'Sets the game as being not actively played
             BtnStart.Text = "Restart" 'Renames start button
-            BtnStart.Enabled = True
+            BtnStart.Enabled = True 'Enables the (re)start button
         ElseIf Row1 = -3 OrElse Row2 = -3 OrElse Row3 = -3 OrElse Column1 = -3 OrElse Column2 = -3 OrElse Column3 = -3 OrElse ForwardS = -3 OrElse BackS = -3 Then
             LblPlayer.Text = "O Wins!"
             GameOver = True
             Playing = False
             BtnStart.Text = "Restart"
             BtnStart.Enabled = True
-        ElseIf CurrentTurn = 9 Then
+        ElseIf CurrentTurn = 9 Then 'Logic for a draw; turn limit is reached yet the above two checks did not succeed
             LblPlayer.Text = "Draw!"
             GameOver = True
             Playing = False
@@ -192,19 +152,24 @@
         Else
             Exit Sub
         End If
-
     End Sub
-
     Private Sub GameBoard()
-        'PlayerTurn 1 for p1 2 for p2
-        'MessageBox.Show(GridColumn.ToString)
-        'MessageBox.Show(GridRow.ToString)
-        If PlayerTurn = 1 Then
+        'This uses the holding variables set upon _Click to approprietly change the value in the array that corrisponds to the chosen square and player turn
+        If PlayerTurn = 1 And Not SpotInUse() Then
             Grid(GridColumn, GridRow) = 1
-        ElseIf PlayerTurn = 2 Then
+        ElseIf PlayerTurn = 2 And Not SpotInUse() Then
             Grid(GridColumn, GridRow) = -1
         End If
-
     End Sub
-
+    Function SpotInUse() As Boolean
+        'This function is checked within GameBoard() to prevent the win-condition array from being changed if the current player selects a square already taken
+        'The similar logic within PlayerMark() only prevents visual changes, not the mechanics change 
+        If CurrentSpot.text <> "-" Then
+            SpotInUse = True
+            Exit Function
+            MessageBox.Show("Please choose a square that has not yet been claimed.")
+        Else
+            SpotInUse = False
+        End If
+    End Function
 End Class
